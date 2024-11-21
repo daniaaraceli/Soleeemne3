aa8d2dc72ba9d5886fffc49eb3c18c48ab91120d
daniaaraceli <daniaaraceli57@gmail.com>
2024-11-21 09:27:01 -0300

    Update untitled12.py

--- untitled12.py
+++ untitled12.py
@@ -4,18 +4,17 @@ import pandas as pd
 import requests
 
 def obtener_datos_api(api_url):
-"""Función que realiza la petición a la API y devuelve un
-DataFrame."""
-response = requests.get(api_url)
-if response.status_code == 200:
-    data = response.json()
-    return pd.DataFrame(data)
-else:
-    st.error('Error al obtener los datos de la API')
-    return None
-# Llamar la función para obtener los datos
-df = obtener_datos_api(api_url)
-# Si hay datos, mostrar el DataFrame, mostrar dataframe con las columna seleccionadas, permitir filtrado y mostrar gráficos.
-if df is not None:
-    # Mostrar las primeras 5 filas del dataframe
-    st.write(df.head())    
+    """Función que realiza la petición a la API y devuelve un DataFrame."""
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        return pd.DataFrame(data)
+    else:
+        st.error('Error al obtener los datos de la API')
+        return None
+    # Llamar la función para obtener los datos
+    df = obtener_datos_api(api_url)
+    # Si hay datos, mostrar el DataFrame, mostrar dataframe con las columna seleccionadas, permitir filtrado y mostrar gráficos.
+    if df is not None:
+        # Mostrar las primeras 5 filas del dataframe
+        st.write(df.head())    

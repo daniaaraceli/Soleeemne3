cf2d1926dd62aad42a43c3714cb6893692eab663
daniaaraceli <daniaaraceli57@gmail.com>
2024-11-22 17:01:48 -0300

    Update untitled12.py

--- untitled12.py
+++ untitled12.py
@@ -48,5 +48,6 @@ if df is not None:
     st.write(df_seleccionado)
     st.write("Estadísticas de las columnas seleccionadas:")
     st.write("Media:",)
+    st.write(df_seleccionado.mean(numeric_only=True))
     st.write("Mediana:",)
     st.write("Desviación estándar:",)

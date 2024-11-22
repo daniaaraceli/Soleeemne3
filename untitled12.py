0f395daffa807b413a096a1f8e1908d789fd767a
daniaaraceli <daniaaraceli57@gmail.com>
2024-11-22 15:49:42 -0300

    Update untitled12.py

--- untitled12.py
+++ untitled12.py
@@ -20,4 +20,18 @@ df = obtener_datos_api(api_url)
 # Si hay datos, mostrar el DataFrame, mostrar dataframe con las columna seleccionadas, permitir filtrado y mostrar gráficos.
 
 if df is not None:
-    st.write(df.head())    
+    st.write(df.head())
+df['Nombre'] = df['name'].apply(lambda x: x.get('common') if isinstance(x, dict) else None)
+df['Región'] = df['region']
+df['Población'] = df['population']
+df['Área (km²)'] = df['area']
+df['Fronteras'] = df['borders'].apply(lambda x: len(x) if isinstance(x, list) else 0)
+df['Idiomas Oficiales'] = df['languages'].apply(lambda x: len(x) if isinstance(x, dict) else 0)
+df['Zonas Horarias'] = df['timezones'].apply(lambda x: len(x) if isinstance(x, list) else 0)
+
+df_cleaned = df[['Nombre', 'Región', 'Población', 'Área (km²)', 'Fronteras', 'Idiomas Oficiales', 'Zonas Horarias']]
+
+# Visualiza el DataFrame resultante
+print(df_cleaned.head())
+
+

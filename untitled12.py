9d2208e6c520e08febafcaf43aa981a95a0a9ba3
daniaaraceli <daniaaraceli57@gmail.com>
2024-11-28 08:56:22 -0300

    Update untitled12.py

--- untitled12.py
+++ untitled12.py
@@ -15,7 +15,7 @@ def obtener_datos_api(api_url):
 
   
 # Llamar la función para obtener los datos
-api_url = "https://restcountries.com/v3.1/all"
+api_url = "https://jsonplaceholder.typicode.com/posts"
 df = obtener_datos_api(api_url)
 # Si hay datos, mostrar el DataFrame, mostrar dataframe con las columna seleccionadas, permitir filtrado y mostrar gráficos.
 

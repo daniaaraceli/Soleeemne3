f380381f92f9526b7d0844ee8d3bb63171106c42
daniaaraceli <daniaaraceli57@gmail.com>
2024-11-28 11:34:52 -0300

    Update untitled12.py

--- untitled12.py
+++ untitled12.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import requests
-import streamlit as st
+  import pandas as pd
+  import requests
+  import streamlit as st
 
-def obtener_datos_api(api_url):
+  def obtener_datos_api(api_url):
     """Función que realiza la petición a la API y devuelve un DataFrame."""
     response = requests.get(api_url)
-  if response.status_code == 200:
+   if response.status_code == 200:
         data = response.json()
         return pd.DataFrame(data)
- else:
-     st.error('Error al obtener los datos de la API')
-     return None
+    else:
+        st.error('Error al obtener los datos de la API')
+        return None
 
 
 # Llamar la función para obtener los datos
